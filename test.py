4d05aa25cd2d942e17d17eb3f4ca53fbad1d8105
oEscal <pedroescaleira@hotmail.com>
2019-12-13 18:05:27 +0000

    certs verified with public key

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import PyKCS11
 import binascii
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives.asymmetric import padding
 
+from cryptography.exceptions import InvalidSignature
 
 ATTR = "CITIZEN AUTHENTICATION CERTIFICATE"
 
@@ -22,8 +25,8 @@ cert_cc = x509.load_der_x509_certificate(
     bytes(session.findObjects([(PyKCS11.CKA_LABEL, ATTR)])[0].to_dict()['CKA_VALUE']),
     default_backend()
 )
-print(cert_cc.subject)
-print(cert_cc.issuer)
+# print(cert_cc.subject)
+# print(cert_cc.issuer)
 
 
 def validate_certificate(certificate):
@@ -49,18 +52,11 @@ def read_cert(file_name):
       pem_data = file.read()
    return x509.load_pem_x509_certificate(pem_data, default_backend())
 
-path = "cc_certificates/roots/"
-all_root_files = [f"{path}{n}" for n in os.listdir(path) if ".pem" in n]
 path = "cc_certificates/"
 all_files = [f"{path}{n}" for n in os.listdir(path) if ".pem" in n]
 
 roots = {}
 certificates = {}
-for fn in all_root_files:
-   cert = read_cert(fn)
-   roots[cert.subject.rfc4514_string()] = cert
-   certificates[cert.subject.rfc4514_string()] = cert
-
 for fn in all_files:
    cert = read_cert(fn)
    certificates[cert.subject.rfc4514_string()] = cert
@@ -71,7 +67,7 @@ def build_issuers(chain, cert):
     issuer = cert.issuer.rfc4514_string()
     subject = cert.subject.rfc4514_string()
 
-    if issuer == subject and subject in roots:
+    if issuer == subject and subject in certificates:
         return True
 
     if issuer in certificates:
@@ -82,6 +78,35 @@ def build_issuers(chain, cert):
 
 
 chain = []
-print(build_issuers(chain, cert_cc))
+chain_completed = build_issuers(chain, cert_cc)
+if chain_completed:
+    for i in range(1, len(chain)):
+        try:
+            subject = chain[i - 1]
+            issuer = chain[i]
+            issuer_public_key = issuer.public_key()
+            issuer_public_key.verify(
+                subject.signature,
+                subject.tbs_certificate_bytes,
+                # Depends on the algorithm used to create the certificate
+                padding.PKCS1v15(),
+                subject.signature_hash_algorithm,
+            )
+        except InvalidSignature:
+            print("Um dos certificados da cadeia não foi assinado pelo seu issuer")
+            break
 
 
+"""
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives.asymmetric import padding
+issuer_public_key = load_pem_public_key(pem_issuer_public_key, default_backend())
+cert_to_check = x509.load_pem_x509_certificate(pem_data_to_check, default_backend())
+issuer_public_key.verify(
+    cert_to_check.signature,
+    cert_to_check.tbs_certificate_bytes,
+    # Depends on the algorithm used to create the certificate
+    padding.PKCS1v15(),
+    cert_to_check.signature_hash_algorithm,
+)
+"""
